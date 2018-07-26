644a6dedf75f1ddc74ae6bab9d8b844b2f014ecc
syed asad <syedasadshahrizvi@gmail.com>
2018-07-26 10:53:25 +0800

    pull test

--- src/main/java/com/antifake/AntifakeApplication.java
+++ src/main/java/com/antifake/AntifakeApplication.java
@@ -11,7 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class AntifakeApplication {
 
 	public static void main(String[] args) {
-		
+		//test
 		SpringApplication.run(AntifakeApplication.class, args);
 	}
 }
